303d4fd9eb4927d00791b18c81a2cf0e7c161327
karen-codes <kcabascangoa@est.ups.edu.ec>
2025-06-16 15:18:22 -0500

    Agregar botones y listas a la pagina

--- script.js
+++ script.js
@@ -31,3 +31,23 @@ function arreglos(){
         console.log('No es una pera');
     }
 }
+document.getElementById("btnSaludar").addEventListener(
+    "click", function(){
+    console.log("Hola");
+    }
+)
+//Modificar
+document.getElementById("btnAgregar").addEventListener(
+    "click", ()=>{
+       // Obtener el elemnto "lista" (ul) del documento HTML
+       let lista = document.getElementById("lista");
+       // Creando un nuevo elemento HTML, li = list item
+       let nuevoItem = document.createElement("li");
+       // Obteniendo el total de ítems de la lista
+       let totalItems = lista.childElementCount;
+       // Agregando un contenido (texto) al item
+       nuevoItem.textContent = `Item ${totalItems + 1}`;
+       // Agregar item a la lista
+       lista.appendChild(nuevoItem);
+    }
+)
