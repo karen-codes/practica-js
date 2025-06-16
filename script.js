d3e94be78a85e1a1a113e568c5b848e35ed26449
karen-codes <kcabascangoa@est.ups.edu.ec>
2025-06-16 15:54:18 -0500

    Agregar palabras a la lista y validacion de datos

--- script.js
+++ script.js
@@ -1,53 +1,80 @@
-//Funcion para mostar un cuadro de dialogo emergente
+// Funcion para mostrar un cuadro de dialogo emergente
 function mostrarMensaje(){
-    alert("Hola mundo :");
+    alert("Hola mundo :)");
 }
 
-//Variables
+// Variables
 function imprimirVariables(){
-    let nombre = "Karen";
-    let edad = 22;
-    //forma tradicional de concatenar cadenas
+    let nombre = "Rodrigo";
+    let edad = 42;
+    // Forma tradicional de concatenar cadenas
     console.log("nombre: ", nombre);
     console.log("edad: ", edad);
-    //forma moderna de concatenar cadenas
-    console.log(`nombre: , ${nombre}, edad ${edad}`);
-
+    // Forma moderna de concatenar cadenas
+    console.log(`nombre: ${nombre}, edad: ${edad}`);
+    // Otros tipos de impresión
     console.error("Error: bla bla bla");
     console.warn("Advertencia: x x x");
 }
-//Funcion para mostar un cuadro de dialogo emergente
+
 function arreglos(){
-    //Arreglos
-    let frutas = ['manzana', 'pera' ,'uva'];
-    //bucles
-    for (let f of frutas){
+    // Arreglos
+    let frutas = ['manzana', 'pera', 'banana'];
+    // Bucle
+    for(let f of frutas){
         console.log(f)
     }
-    //condicionales
-    if (frutas [1]==='pera'){
+    // Condicionales
+    if (frutas[1] === 'pera'){
         console.log("La segunda fruta es una pera");
     }else{
         console.log('No es una pera');
     }
 }
+// Agregar evente a un elemento HTML
 document.getElementById("btnSaludar").addEventListener(
-    "click", function(){
-    console.log("Hola");
+    "click", function () {
+        console.log("Hola!");
     }
-)
-//Modificar
+);
+
+// Modificar el HTML a través del DOM
 document.getElementById("btnAgregar").addEventListener(
-    "click", ()=>{
-       // Obtener el elemnto "lista" (ul) del documento HTML
-       let lista = document.getElementById("lista");
-       // Creando un nuevo elemento HTML, li = list item
-       let nuevoItem = document.createElement("li");
-       // Obteniendo el total de ítems de la lista
-       let totalItems = lista.childElementCount;
-       // Agregando un contenido (texto) al item
-       nuevoItem.textContent = `Item ${totalItems + 1}`;
-       // Agregar item a la lista
-       lista.appendChild(nuevoItem);
+    "click", () => {
+        // Obtener el elemnto "lista" (ul) del documento HTML
+        let lista = document.getElementById("lista");
+        // Creando un nuevo elemento HTML, li = list item
+        let nuevoItem = document.createElement("li");
+        // Obteniendo el total de ítems de la lista
+        let totalItems = lista.childElementCount;
+        // Agregando un contenido (texto) al item
+        nuevoItem.textContent = `Item ${totalItems + 1}`;
+        // Agregar item a la lista
+        lista.appendChild(nuevoItem);
     }
-)
+);
+
+document.getElementById("btnAddPalabra").addEventListener(
+    "click", addPalabraLista
+);
+
+function addPalabraLista(){
+    // Obtener el input del HTML
+    let input = document.getElementById("txtEntrada");
+    // Obtner el valor del input
+    const texto = input.value.trim();
+    if (texto === "") return;
+    // Transformar a mayusculas
+    const textMayuscula = texto.toUpperCase();
+    // Agregar item a la lista
+    let lista = document.getElementById("lstPalabras");
+    // Creando un nuevo elemento HTML, li = list item
+    let nuevoItem = document.createElement("li");
+    // Agregando un contenido (texto) al item
+    nuevoItem.textContent = textMayuscula;
+    // Agregar item a la lista
+    lista.appendChild(nuevoItem);
+    // limpiar y establecer foco
+    input.value = "";
+    input.focus();
+}
